46d3272baee97d7a01bf14c3dcb3111f87e1fdeb
TacoMilitary <wlolams@outlook.com>
2026-02-23 20:50:02 -0600

    (Easy 2) Tip Calculator (final)

--- rb101-rb119-small-problems/easy2/tip_calculator/tip_calculator.rb
+++ rb101-rb119-small-problems/easy2/tip_calculator/tip_calculator.rb
@@ -7,7 +7,7 @@ TEXTS = YAML.load_file('texts.yml')
 USD_DECIMALS = 2
 PERCENT_MULT = 100.0
 
-SCREEN_DIVIDER = "\n----------"
+SCREEN_DIVIDER = "\n----------\n "
 
 TEXT_NOT_FOUND = '! TEXT NOT FOUND !'
 
@@ -19,11 +19,20 @@ def clear_screen
   system('clear') || system('cls') || divide_screen
 end
 
-def n_to_usd(n)
-  formatted_num = n.to_f.round(USD_DECIMALS).to_s
+def display_error(message = 'Unknown error!')
+  divide_screen
+
+  puts "[ERROR]: #{message}"
+
+  divide_screen
+end
+
+def n_to_usd(num)
+  formatted_num = num.to_f.round(USD_DECIMALS).to_s
 
   decimal_index = formatted_num.index('.')
-  formatted_num.concat('0') if formatted_num[(decimal_index + 1)..-1].length == 1
+  missing_number = formatted_num[(decimal_index + 1)..].length == 1
+  formatted_num.concat('0') if missing_number
 
   "$#{formatted_num}"
 end
@@ -47,19 +56,52 @@ def get_txt(text_to_find)
   TEXTS[text_to_find] || TEXT_NOT_FOUND
 end
 
-def run
-  clear_screen
-  puts get_txt('welcome')
+def only_numbers?(str)
+  str.each_char.all? { |c| (('0'..'9').include?(c) || c == '.') }
+end
 
-  divide_screen
-  bill_amount = 500.0
+def bill_input_error?(user_input)
+  if user_input.to_f.negative?
+    get_txt('zero_bill_error')
+  elsif user_input.empty? || !only_numbers?(user_input)
+    get_txt('invalid_error')
+  end
+end
 
-  divide_screen
-  tip_percent = 0.15
+def tip_input_error?(user_input)
+  if user_input.to_f.negative?
+    get_txt('negative_tip_error')
+  elsif user_input.empty? || !only_numbers?(user_input)
+    get_txt('invalid_error')
+  end
+end
 
-  tip_total = bill_amount * tip_percent
-  bill_total =  bill_amount + tip_total
+def prompt_bill_amount
+  loop do
+    user_input = prompt_user(get_txt('bill_ask')).delete_prefix('$')
 
+    error = bill_input_error?(user_input)
+
+    return user_input.to_f unless error
+
+    display_error(error)
+  end
+end
+
+def prompt_tip_percent
+  loop do
+    user_input = prompt_user(get_txt('tip_ask')).delete_suffix('%')
+
+    error = tip_input_error?(user_input)
+
+    user_percent = user_input.to_f / PERCENT_MULT
+    return user_percent unless error
+
+    display_error(error)
+  end
+end
+
+def display_receipt(tip_total, bill_total, tip_percent)
   receipt_text = fill_placeholders(
     get_txt('receipt'),
     'tip' => n_to_usd(tip_total),
@@ -68,6 +110,22 @@ def run
   )
 
   puts receipt_text
+end
+
+def run
+  clear_screen
+  puts get_txt('welcome')
+
+  divide_screen
+  bill_amount = prompt_bill_amount
+
+  divide_screen
+  tip_percent = prompt_tip_percent
+
+  tip_total = bill_amount * tip_percent
+  bill_total = bill_amount + tip_total
+
+  display_receipt(tip_total, bill_total, tip_percent)
 end
 
 run
