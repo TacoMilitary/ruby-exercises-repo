f3c3d6fdf349635225e242195d134b19f73c7534
TacoMilitary <wlolams@outlook.com>
2026-03-01 18:41:26 -0600

    (Easy 3) Palindromic Numbers

---
+++ rb101-rb119-small-problems/easy3/palindromic_numbers/palindrome_num.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+def palindromic_number?(number)
+  number.to_s == number.to_s.reverse
+end
+
+p palindromic_number?(34543) == true
+p palindromic_number?(123210) == false
+p palindromic_number?(22) == true
+p palindromic_number?(5) == true
