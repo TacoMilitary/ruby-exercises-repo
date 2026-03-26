d00c32ec04a357633fdac4ce40ebb76186a7d246
TacoMilitary <wlolams@outlook.com>
2026-03-25 21:16:00 -0500

    (The Spot - RB101) Problem 64

---
+++ the_spot/rb101/p64.rb
@@ -0,0 +1,27 @@
+#  frozen_string_literal: true
+
+input_hash = { a: [1, 2, 3], b: [4, 5, 6] }
+# Transformed hash: { a: [0, 0, 0], b: [0, 0, 0] }
+
+def non_mutating_transform(hash)
+  hash.transform_values do |value|
+    return value unless value.is_a? Array
+    Array.new(value.size, 0)
+  end
+end
+
+def partial_mutating_transform(hash)
+  hash.each do |key, value|
+    hash[key] = value.map { 0 } if value.is_a? Array
+  end
+end
+
+def mutating_transform(hash)
+  hash.each do |_, value|
+    value.map! { 0 } if value.is_a? Array
+  end
+end
+
+p input_hash
+p non_mutating_transform input_hash
+p input_hash
