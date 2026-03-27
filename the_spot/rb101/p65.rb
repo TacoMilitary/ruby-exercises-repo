08a8ffbcbee0e1ab774588980b1d99af9a39bde1
TacoMilitary <wlolams@outlook.com>
2026-03-27 16:45:42 -0500

    (The Spot - RB101) Problem 65 Refactor

--- the_spot/rb101/p65.rb
+++ the_spot/rb101/p65.rb
@@ -5,10 +5,27 @@ library = {
   "1984" => { author: "George Orwell", copies: 5 },
 }
 
+def book_valid_title?(book_title)
+  book_title.is_a?(String) && !book_title.strip.empty?
+end
+
 def add_new_book(book_collection, title, author_name = 'Unknown', amount = 1)
-  return puts('You must name your book!') unless title
-  book_hash = { author: author_name, copies: amount}
+  unless book_valid_title?(title)
+    puts('You must name your book!')
+    return
+  end
+  book_hash = { author: author_name, copies: amount }
   book_collection[title] = book_hash
+  puts "Added #{title} to the library!"
+end
+
+def delete_book(book_collection, book_title)
+  book_collection.delete(book_title)
+  puts "#{book_title} has been removed from the library!"
+end
+
+def delete_if_no_copies(book_collection, book_title, copies)
+  delete_book(book_collection, book_title) if copies <= 0
 end
 
 def checkin(book_collection, book_title)
@@ -17,19 +34,14 @@ def checkin(book_collection, book_title)
     book[:copies] += 1
   else
     add_new_book(book_collection, book_title)
-    puts "Added #{book_title} to the library!"
   end
 end
 
 def checkout(book_collection, book_title)
   book = book_collection[book_title]
   if book
-    if book[:copies] <= 1
-      book_collection[book_title] = nil
-      puts "#{book_title} has been removed from the library!"
-      return
-    end
     book[:copies] -= 1
+    delete_if_no_copies(book_collection, book_title, book[:copies])
   else
     puts "Sorry, there's no book named #{book_title}!"
   end
