7f0203d79fe9630ea7f156d83beeef1089f49efb
TacoMilitary <wlolams@outlook.com>
2026-05-12 17:23:34 -0500

    (Easy 4) Short Long Short (refactor)

--- rb101-rb119-small-problems/easy4/short_long_short/short_long_short.rb
+++ rb101-rb119-small-problems/easy4/short_long_short/short_long_short.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 def short_long_short(string1, string2)
-  short_first = [string1, string2].sort_by(&:length)
-
-  short_first.first + short_first.last + short_first.first
+  shortest, longest = [string1, string2].sort_by(&:length)
+  shortest + longest + shortest
 end
 
 p short_long_short('abc', 'defgh') == 'abcdefghabc'
