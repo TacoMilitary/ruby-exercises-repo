7c05c263162ccfee6e6838b6dbdc349e030c6279
TacoMilitary <wlolams@outlook.com>
2026-05-13 18:05:44 -0500

    (Easy 4) Convert a String to a Signed Number

--- rb101-rb119-small-problems/easy4/convert_string_number/convert_string.rb
+++ rb101-rb119-small-problems/easy4/convert_string_number/convert_string.rb
@@ -2,8 +2,13 @@
 
 NUMBER_CHARS = ('0'..'9').to_a.freeze
 
+def remove_num_prefix(string)
+  string.delete_prefix('-').delete_prefix('+')
+end
+
 def string_to_integer(str_number = '0')
-  characters = str_number.chars.reverse
+  clean_str = remove_num_prefix(str_number)
+  characters = clean_str.chars.reverse
 
   converted_integer = 0
   num_place = 0
@@ -11,13 +16,17 @@ def string_to_integer(str_number = '0')
     char = characters[num_place]
     my_digit = NUMBER_CHARS.index(char)
 
-    converted_integer += my_digit * (10 ** num_place)
+    converted_integer += my_digit * (10**num_place)
 
     num_place += 1
   end
 
-  converted_integer
+  sign = str_number.start_with?('-') ? -1 : 1
+  converted_integer * sign
 end
 
 p string_to_integer('4321') == 4321
 p string_to_integer('570') == 570
+p string_to_integer('4321') == 4321
+p string_to_integer('-570') == -570
+p string_to_integer('+100') == 100
