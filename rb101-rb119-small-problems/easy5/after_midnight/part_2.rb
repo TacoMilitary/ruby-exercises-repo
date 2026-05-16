e082d243a0a05be66f1d06fde883318eddaa15d4
TacoMilitary <wlolams@outlook.com>
2026-05-16 14:18:00 -0500

    (Easy 5) After Midnight Part 2 fixes

--- rb101-rb119-small-problems/easy5/after_midnight/part_2.rb
+++ rb101-rb119-small-problems/easy5/after_midnight/part_2.rb
@@ -2,16 +2,18 @@
 
 MINUTES_PER_HOUR = 60
 
+MINUTES_PER_DAY = 1440
+
 def after_midnight(clock_time)
   hours_minutes = clock_time.split(':').map(&:to_i)
   minutes_from_hours = hours_minutes.first * MINUTES_PER_HOUR
-  (minutes_from_hours + hours_minutes.last) % 1440
+  (minutes_from_hours + hours_minutes.last) % MINUTES_PER_DAY
 end
 
 def before_midnight(clock_time)
   hours_minutes = clock_time.split(':').map(&:to_i)
-  minutes_from_hours = hours_minutes.first * MINUTES_PER_HOUR
-  (minutes_from_hours - hours_minutes.last) % 1440
+  minutes_from_hours = MINUTES_PER_DAY - hours_minutes.first * MINUTES_PER_HOUR
+  (minutes_from_hours - hours_minutes.last) % MINUTES_PER_DAY
 end
 
 p after_midnight('00:00') == 0
@@ -20,3 +22,8 @@ p after_midnight('12:34') == 754
 p before_midnight('12:34') == 686
 p after_midnight('24:00') == 0
 p before_midnight('24:00') == 0
+
+p before_midnight('23:59') == 1
+p before_midnight('00:01') == 1439
+p before_midnight('13:00') == 660
+
