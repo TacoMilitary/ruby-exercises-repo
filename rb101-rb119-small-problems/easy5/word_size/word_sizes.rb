64bdf2275386eb8e1d6ab4b0d805f2ce695edf19
Brandon <brandon@localhost.localdomain>
2026-05-17 13:17:54 -0500

    (Easy 5) Word Sizes

---
+++ rb101-rb119-small-problems/easy5/word_size/word_sizes.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+def word_sizes(sentence)
+  sizes_hash = Hash.new(0)
+
+  sentence.split.each { |word| sizes_hash[word.length] += 1 }
+
+  sizes_hash
+end
+
+p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
+p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 1, 7 => 2 }
+p word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
+p word_sizes('') == {}
