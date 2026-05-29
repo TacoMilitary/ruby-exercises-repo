6d3e20b5370bc1f7f3cd7e363ae3b12fd87e8c85
TacoMilitary <wlolams@outlook.com>
2026-05-29 14:03:26 -0500

    (Easy 8) Palindromci Substrings

--- rb101-rb119-small-problems/easy8/palindromic_substrings/palindromic_substrings.rb
+++ rb101-rb119-small-problems/easy8/palindromic_substrings/palindromic_substrings.rb
@@ -12,18 +12,23 @@ def substrings(string)
   substrings_array.flatten
 end
 
+def palindrome?(string)
+  # string = string.delete '^A-Za-z'
+  string.size > 1 && string.casecmp?(string.reverse)
+end
+
 def palindromes(string)
   all_substrings = substrings string
-  palindromics = all_substrings.keep_if { |substr| substr.size > 1 && substr == substr.reverse }
+  all_substrings.keep_if { |substr| substr.size > 1 && substr == substr.reverse }
 end
 
 p palindromes('abcd') == []
-p palindromes('madam') == ['madam', 'ada']
+p palindromes('madam') == %w[madam ada]
 p palindromes('hello-madam-did-madam-goodbye') == [
   'll', '-madam-', '-madam-did-madam-', 'madam', 'madam-did-madam', 'ada',
   'adam-did-mada', 'dam-did-mad', 'am-did-ma', 'm-did-m', '-did-', 'did',
   '-madam-', 'madam', 'ada', 'oo'
 ]
-p palindromes('knitting cassettes') == [
-  'nittin', 'itti', 'tt', 'ss', 'settes', 'ette', 'tt'
+p palindromes('knitting cassettes') == %w[
+  nittin itti tt ss settes ette tt
 ]
