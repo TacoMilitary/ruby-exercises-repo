299947ce0fd5057d1278841d4d35b1cccffc2fa1
TacoMilitary <wlolams@outlook.com>
2026-05-30 16:19:22 -0500

    (Easy 8) Double Char part 2

---
+++ rb101-rb119-small-problems/easy8/double_char/part2.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+CONSONANTS = ('a'..'z').reject { |letter| 'aeiou'.include?(letter) }.freeze
+
+def consonant?(chr)
+  CONSONANTS.include? chr.downcase
+end
+
+def double_consonants(string)
+  string.chars.map! { |chr| consonant?(chr) ? chr * 2 : chr }.join
+end
+
+p double_consonants('String') == 'SSttrrinngg'
+p double_consonants('Hello-World!') == 'HHellllo-WWorrlldd!'
+p double_consonants('July 4th') == 'JJullyy 4tthh'
+p double_consonants('') == ''
