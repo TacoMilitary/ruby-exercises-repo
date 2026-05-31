40e9114e3f74fbf213d99c11b2f7f8b170baca3c
Brandon <brandon@localhost.localdomain>
2026-05-31 12:59:46 -0500

    (Easy 8) Reverse It part 2

---
+++ rb101-rb119-small-problems/easy8/reverse_it/part2.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+def reverse_words(sentence)
+  words = sentence.split.map { |word| word.size >= 5 ? word.reverse : word }
+  words.join ' '
+end
+
+puts reverse_words('Professional')          # => lanoisseforP
+puts reverse_words('Walk around the block') # => Walk dnuora the kcolb
+puts reverse_words('Launch School')         # => hcnuaL loohcS
