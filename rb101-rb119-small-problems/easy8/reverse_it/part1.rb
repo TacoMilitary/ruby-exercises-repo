11db7a98c218cf367ddb84df6bf2577c8098e038
Brandon <brandon@localhost.localdomain>
2026-05-31 12:41:07 -0500

    (Easy 8) Reverse It part 1

---
+++ rb101-rb119-small-problems/easy8/reverse_it/part1.rb
@@ -0,0 +1,24 @@
+# frozen_string_Literal: true
+
+=begin
+def reverse_sentence(sentence)
+  sentence.split.reverse.join ' '
+end
+=end
+
+def reverse_sentence(sentence)
+  words = sentence.split
+  return '' unless words.any? { |word| /[^\s]/.match word }
+
+  words_count = words.size
+  return sentence if words_count <= 1
+
+  mid_index = words_count / 2 - 1
+  0.upto(mid_index) { |index| words[index], words[-1 - index] = words[-1 - index], words[index] }
+  words.join ' '
+end
+
+puts reverse_sentence('Hello World') == 'World Hello'
+puts reverse_sentence('Reverse these words') == 'words these Reverse'
+puts reverse_sentence('') == ''
+puts reverse_sentence('    ') == '' # Any number of spaces results in ''
