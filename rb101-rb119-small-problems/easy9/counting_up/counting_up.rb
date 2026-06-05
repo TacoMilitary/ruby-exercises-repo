ba4c2494019682766cd5777d5b8b22ff5cfb681c
TacoMilitary <wlolams@outlook.com>
2026-06-05 16:24:36 -0500

    (Easy 9) Counting Up (further exploration)

--- rb101-rb119-small-problems/easy9/counting_up/counting_up.rb
+++ rb101-rb119-small-problems/easy9/counting_up/counting_up.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 def sequence(upto_number)
-  upto_number > 0 ? (1..upto_number).to_a : [1]
+  return [0] if upto_number.zero?
+  upto_number.positive? ? (1..upto_number).to_a : (upto_number..-1).to_a.reverse
 end
 
 p sequence(5) == [1, 2, 3, 4, 5]
 p sequence(3) == [1, 2, 3]
 p sequence(1) == [1]
+p sequence(0) == [0]
+p sequence(-1) == [-1]
+p sequence(-6) == [-1, -2, -3, -4, -5, -6]
