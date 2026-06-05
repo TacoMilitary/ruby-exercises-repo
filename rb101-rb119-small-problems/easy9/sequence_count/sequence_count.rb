260e903e59198d0fddad82fa9f7b68cacf9bdf04
TacoMilitary <wlolams@outlook.com>
2026-06-05 16:44:21 -0500

    (Easy 9) Sequence Count

---
+++ rb101-rb119-small-problems/easy9/sequence_count/sequence_count.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+def sequence(count, multiples_of)
+  current_num = 0
+  Array.new(count) { current_num += multiples_of }
+end
+
+p sequence(5, 1) == [1, 2, 3, 4, 5]
+p sequence(4, -7) == [-7, -14, -21, -28]
+p sequence(3, 0) == [0, 0, 0]
+p sequence(0, 1000000) == []
