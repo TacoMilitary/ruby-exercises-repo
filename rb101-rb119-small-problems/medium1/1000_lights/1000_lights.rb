e216e0c64b5003ae83e355cb630adb3407fa520e
TacoMilitary <wlolams@outlook.com>
2026-06-06 01:13:03 -0500

    (Medium 1) 1000 Lights (Further Exploration)

--- rb101-rb119-small-problems/medium1/1000_lights/1000_lights.rb
+++ rb101-rb119-small-problems/medium1/1000_lights/1000_lights.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+def output_light_state(lights)
+  text = case lights.size
+         when 0 then 'No lights are left on.'
+         when 1 then "Only light #{lights.first} is left on."
+         when 2 then "Only lights #{lights.first} and #{lights.last} are left on."
+         else
+           lights_string = "#{lights[..-2].join(', ')} and #{lights.last}"
+           "Lights #{lights_string} are left on."
+         end
+  puts text
+end
+
 def lights_toggle(times)
   lights = [true] * times
   (2..times).each do |iteration|
@@ -8,9 +20,10 @@ def lights_toggle(times)
     end
   end
 
-  lights.filter_map.with_index { |status, light_num| light_num.next if status }
+  lights_on = lights.filter_map.with_index { |status, light_num| light_num.next if status }
+  output_light_state(lights_on)
+  lights_on
 end
-
 
 p lights_toggle(5) == [1, 4]
 p lights_toggle(10) == [1, 4, 9]
