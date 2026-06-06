0287324ef39e24ce62b60bdf7492803ad3dc3854
TacoMilitary <wlolams@outlook.com>
2026-06-06 00:42:17 -0500

    (Medium 1) Rotation (Part 2) (solved on paper, but not in practice)

---
+++ rb101-rb119-small-problems/medium1/rotation/part2.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+def rotate_rightmost_digits(number, reverse_count)
+  digits = number.digits.reverse
+  digits.push [digits.pop(reverse_count)].flatten.reverse
+  digits.join.to_i
+end
+
+p rotate_rightmost_digits(735291, 1) == 735291
+p rotate_rightmost_digits(735291, 2) == 735219
+p rotate_rightmost_digits(735291, 3) == 735912
+p rotate_rightmost_digits(735291, 4) == 732915
+p rotate_rightmost_digits(735291, 5) == 752913
+p rotate_rightmost_digits(735291, 6) == 352917
