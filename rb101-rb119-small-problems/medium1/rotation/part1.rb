0ccb5dee7660805bf9d2492da68f739688c65096
TacoMilitary <wlolams@outlook.com>
2026-06-06 00:22:02 -0500

    (Medium 1) Rotation (Part 1)

---
+++ rb101-rb119-small-problems/medium1/rotation/part1.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+def rotate_array(array)
+  rotated_array = array.dup
+  rotated_array.push rotated_array.shift
+end
+
+p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
+p rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
+p rotate_array(['a']) == ['a']
+
+x = [1, 2, 3, 4]
+p rotate_array(x) == [2, 3, 4, 1]   # => true
+p x == [1, 2, 3, 4]                 # => true
