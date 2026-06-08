1dd37ef1781dde3ed619d80a400d9793294a5a91
TacoMilitary <wlolams@outlook.com>
2026-06-08 17:04:47 -0500

    (Medium 1) Diamonds

--- rb101-rb119-small-problems/medium1/diamonds/diamonds.rb
+++ rb101-rb119-small-problems/medium1/diamonds/diamonds.rb
@@ -1,15 +1,26 @@
 # frozen_string_literal: true
 
-def trapezoid()
-
-end
-
-def diamond(size)
-  half_size = (size / 2.0).ceil
-  size.times do |row_number|
-    row_visual = '*' * [1, row_number.next].min
-    puts row_visual.rjust(half_size)
+def diamond_row_size(row, size, middle_row)
+  if row > middle_row
+    size - ((row - middle_row) * 2)
+  else
+    row * 2 - 1
   end
 end
 
+def diamond(size)
+  diamond_str = +''
+  middle_row = (size / 2.0).ceil
+  (1..size).each do |row|
+    row_size = diamond_row_size(row, size, middle_row)
+    row_half_size = row_size / 2
+    spaces = ' ' * (middle_row - row_half_size.next)
+    diamond_str << "#{spaces}#{'*' * row_size}\n"
+  end
+
+  puts diamond_str
+end
+
 diamond(3)
+
+diamond(9)
