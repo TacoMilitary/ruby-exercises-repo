e14f95632907a487be42359333d2bb93d05c57f7
TacoMilitary <wlolams@outlook.com>
2026-06-13 02:56:26 -0500

    (Medium 1) Word to Digit refactor

--- rb101-rb119-small-problems/medium1/word_to_digit/word_to_digit.rb
+++ rb101-rb119-small-problems/medium1/word_to_digit/word_to_digit.rb
@@ -3,8 +3,7 @@
 DIGIT_WORDS = %w[zero one two three four five six seven eight nine].freeze
 
 def word_to_digit(sentence)
-  DIGIT_WORDS.each_with_index { |word, digit| sentence.gsub!(/\b#{word}\b/i, digit.to_s) }
-  sentence
+  sentence.gsub(/#{DIGIT_WORDS.join('|')}/i) { |word| DIGIT_WORDS.index(word) }
 end
 
 p word_to_digit('Please call me at five five five one two three four. Thanks.') == 'Please call me at 5 5 5 1 2 3 4. Thanks.'
