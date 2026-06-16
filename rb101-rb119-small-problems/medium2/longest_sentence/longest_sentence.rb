7db4d4c661b4dc06154134944f6cb061ba5635f5
TacoMilitary <wlolams@outlook.com>
2026-06-15 23:54:40 -0500

    (Medium 2) Longest Sentence (solution 2)

--- rb101-rb119-small-problems/medium2/longest_sentence/longest_sentence.rb
+++ rb101-rb119-small-problems/medium2/longest_sentence/longest_sentence.rb
@@ -1,16 +1,26 @@
 # frozen_string_literal: true
 
-def longest_sentence(text)
+def solution1(text)
   sentences = text.scan(/[^\s].*?[.!?]+/m).map do |sentence|
-    { string: sentence, size: sentence.scan(/[^\s.?!]+/).size }
+    { size: sentence.scan(/[^\s.?!]+/).size, string: sentence }
   end
 
-  puts sentences.max_by { |sentence_info| sentence_info[:size] }.values.reverse.join("\n\n")
+  puts sentences.max_by { |sentence_info| sentence_info[:size] }.values.join("\n--------\n")
+end
+
+# Whitespace not really needed to be included in the regex,
+# since later we use #split on the strings, which does it anyway.
+# Left it to be explicit.
+
+def solution2(text)
+  sentences = text.split(/[.!?]+\s*/).map { |sentence| {size: sentence.split.size, string: sentence } }
+  longest = sentences.max_by { |sentence_info| sentence_info[:size] }
+  puts "Words: #{longest[:size]}\n--------\n#{longest[:string]}"
 end
 
 case1 = IO.read 'case1.txt'
 case2 = IO.read 'case2.txt'
 
-longest_sentence(case1)
+solution1(case1)
 puts "\n--------\n "
-longest_sentence(case2)
+solution2(case2)
