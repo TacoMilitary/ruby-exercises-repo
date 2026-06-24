50548c40db363dbcfa0dc52c6ce8a07b018093f4
TacoMilitary <wlolams@outlook.com>
2026-06-23 20:18:32 -0500

    (Medium 2) Tri-Angles (solution 1)

---
+++ rb101-rb119-small-problems/medium2/tri-angles/triangles.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+def valid_triangle?(angles)
+  return false unless angles.sum == 180
+  return false if angles.any? { |deg| deg <= 0 }
+  true
+end
+
+def triangle(*angles)
+  return :invalid unless valid_triangle?(angles)
+  if angles.include? 90
+    :right
+  elsif angles.any? { |deg| deg > 90 }
+    :obtuse
+  else
+    :acute
+  end
+end
+
+p triangle(60, 70, 50) == :acute
+p triangle(30, 90, 60) == :right
+p triangle(120, 50, 10) == :obtuse
+p triangle(0, 90, 90) == :invalid
+p triangle(50, 50, 50) == :invalid
