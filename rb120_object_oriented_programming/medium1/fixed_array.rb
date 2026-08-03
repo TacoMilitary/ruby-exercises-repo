c2a69d506be043dde42e3cc4522403e12050df4e
Brandon <brandon@localhost.localdomain>
2026-08-03 18:18:04 -0500

    (Medium 1) Fixed Array & Students

---
+++ rb120_object_oriented_programming/medium1/fixed_array.rb
@@ -0,0 +1,85 @@
+class FixedArray
+  def initialize(fixed_size)
+    @fixed_size = fixed_size
+    @elements = [nil] * fixed_size
+  end
+
+  def to_s
+    elements.to_s
+  end
+
+  def inspect
+    elements.inspect
+  end
+
+  def to_a
+    elements.dup
+  end
+
+  def size
+    @fixed_size
+  end
+
+  def [](index)
+    raise(IndexError) unless within_range?(index)
+    elements[index]
+  end
+
+  def []=(index, new_value)
+    raise(IndexError) unless within_range?(index)
+    elements[index] = new_value
+  end
+
+  private
+
+  attr_reader :elements
+
+  def within_range?(index)
+    [(0...size), (-size..-1)].any? { |range| range.include? index }
+  end
+end
+
+fixed_array = FixedArray.new(5)
+puts fixed_array[3] == nil
+puts fixed_array.to_a == [nil] * 5
+
+fixed_array[3] = 'a'
+puts fixed_array[3] == 'a'
+puts fixed_array.to_a == [nil, nil, nil, 'a', nil]
+
+fixed_array[1] = 'b'
+puts fixed_array[1] == 'b'
+puts fixed_array.to_a == [nil, 'b', nil, 'a', nil]
+
+fixed_array[1] = 'c'
+puts fixed_array[1] == 'c'
+puts fixed_array.to_a == [nil, 'c', nil, 'a', nil]
+
+fixed_array[4] = 'd'
+puts fixed_array[4] == 'd'
+puts fixed_array.to_a == [nil, 'c', nil, 'a', 'd']
+puts fixed_array.to_s == '[nil, "c", nil, "a", "d"]'
+
+puts fixed_array[-1] == 'd'
+puts fixed_array[-4] == 'c'
+
+begin
+  fixed_array[6]
+  puts false
+rescue IndexError
+  puts true
+end
+
+begin
+  fixed_array[-7] = 3
+  puts false
+rescue IndexError
+  puts true
+end
+
+begin
+  fixed_array[7] = 3
+  puts false
+rescue IndexError
+  puts true
+end
